a57a538c210446e70fef786f23f6ff398b1040be
Gareth Williams <gaz492@gmail.com>
2017-04-16 23:18:42 +0100

    command registry

--- modules/bot/commandManager.js
+++ modules/bot/commandManager.js
@@ -7,42 +7,80 @@ const colour = require('irc-colors');
 
 const mcCheck = require('../core/mcCheck');
 
-module.exports = {
-    onCommand: function (client, command, to, from, args){
-        if (command === "help") {
-            if (args.length >= 1){
-                client.notice(from, "Nothing here yet");
-            }else{
-                // var printCmdList = [];
-                // for (var prop in cmdList) {
-                //     if (cmdList.hasOwnProperty(prop)) {
-                //         var splitCmd = cmdList[prop].split(",");
-                //         printCmdList = printCmdList.concat(splitCmd);
-                //     }
-                // }
-                client.notice(from, "Usage: .help <command>");
-                client.notice(from, "Available Commands: help");
-            }
-        }
-        else if(command === "paid"){
-            mcCheck.checkPlayerName(args[0], function(data){
-                if(data){
-                    let uuid = data.id;
-                    let name = data.name;
+let cmdList = {"commandManager": "help"};
+let isInit = false;
 
-                    client.say(to, "Username: " + colour.bold(name) + ", UUID: " + colour.bold(uuid) + ", Paid: " + colour.green.bold("TRUE"));
-                }else{
-                    client.say(to, "No data for " + colour.bold(args[0]));
-                }
-            });
-        }
-        else if(command === "mcstatus"){
-            mcCheck.checkMojangStatus(function(data){
-                client.say(to, data)
-            })
-        }
-        else {
-            client.say(to, "Command not found");
+init();
+
+function findValue(module, value, object) {
+    for (let prop in object) {
+        if (object.hasOwnProperty(prop) && object[prop] === value) {
+            return true;
         }
     }
+}
+
+function init(){
+    if(!isInit){
+        registerCmd("commandManager", "help");
+        registerCmd("mcCheck", "paid");
+        registerCmd("mcCheck", "mcstatus");
+    }
+    isInit = true;
+}
+
+function registerCmd(module, command){
+    if(!cmdList.hasOwnProperty(module)){
+        if(!findValue(module, command, cmdList)){
+            cmdList[module] = command;
+        }
+    }else{
+        if(!findValue(module, command, cmdList)){
+            cmdList.module = command;
+        }
+    }
+}
+
+function onCommand(client, command, to, from, args){
+    if (command === "help") {
+        if (args.length >= 1){
+            client.notice(from, "Nothing here yet");
+        }else{
+            let printCmdList = [];
+            for (let prop in cmdList) {
+                if (cmdList.hasOwnProperty(prop)) {
+                    let splitCmd = cmdList[prop].split(",");
+                    printCmdList = printCmdList.concat(splitCmd);
+                }
+            }
+            client.notice(from, "Usage: .help <command>");
+            client.notice(from, "Available Commands: " + printCmdList.toString());
+        }
+    }
+    else if(command === "paid"){
+        mcCheck.checkPlayerName(args[0], function(data){
+            if(data){
+                let uuid = data.id;
+                let name = data.name;
+
+                client.say(to, "Username: " + colour.bold(name) + ", UUID: " + colour.bold(uuid) + ", Paid: " + colour.green.bold("TRUE"));
+            }else{
+                client.say(to, "No data for " + colour.bold(args[0]));
+            }
+        });
+    }
+    else if(command === "mcstatus"){
+        mcCheck.checkMojangStatus(function(data){
+            client.say(to, data)
+        })
+    }
+    else {
+        client.say(to, "Command not found");
+    }
+}
+
+module.exports = {
+    registerCmd: registerCmd,
+    onCommand: onCommand
+
 };
